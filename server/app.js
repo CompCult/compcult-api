3dcebfc0f0cdb42a405c29597fca75f31188d3cc
danielrcoura <daniel.coura@ccc.ufcg.edu.br>
2019-02-21 17:41:05 -0300

    Modulariza routes

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
-var express = require('express');
+const express = require('express');
 
-var app = express();
+const app = express();
 
 require('./startup/logger')(app);
 require('./startup/cors')(app);
@@ -9,41 +9,7 @@ require('./startup/db')();
 
 app.get('/', (req, res) => res.send('This API is running, baby!'));
 
-var general = require('./routes/general.js');
-var users = require('./routes/user.js');
-var missions = require('./routes/mission.js');
-var missions_answer = require('./routes/mission_answer.js');
-var quizzes = require('./routes/quiz.js');
-var quizzes_answer = require('./routes/quiz_answer.js');
-var groups = require('./routes/group.js');
-var group_member = require('./routes/group_member.js');
-var post = require('./routes/post.js');
-var reaction = require('./routes/reaction.js');
-var tree = require('./routes/tree.js');
-var tree_request = require('./routes/tree_request.js');
-var tree_type = require('./routes/tree_type.js');
-var appointment = require('./routes/appointment.js');
-var appointment_request = require('./routes/appointment_request.js');
-var places = require('./routes/place.js');
-var analytics = require('./routes/analytics');
-
-app.use('/general', general);
-app.use('/users', users);
-app.use('/missions', missions);
-app.use('/missions_answers', missions_answer);
-app.use('/quizzes', quizzes);
-app.use('/quiz_answers', quizzes_answer);
-app.use('/groups', groups);
-app.use('/group_members', group_member);
-app.use('/posts', post);
-app.use('/reactions', reaction);
-app.use('/trees', tree);
-app.use('/tree_requests', tree_request);
-app.use('/tree_types', tree_type);
-app.use('/appointment', appointment);
-app.use('/appointment_requests', appointment_request);
-app.use('/places', places);
-app.use('/analytics', analytics);
+require('./startup/routes')(app);
 
 require('./startup/errors')(app);
 
---
+++ server/startup/routes.js
@@ -0,0 +1,37 @@
+const general = require('../routes/general.js');
+const users = require('../routes/user.js');
+const missions = require('../routes/mission.js');
+const missionsAnswer = require('../routes/missionAnswer.js');
+const quizzes = require('../routes/quiz.js');
+const quizzesAnswer = require('../routes/quizAnswer.js');
+const groups = require('../routes/group.js');
+const groupMember = require('../routes/groupMember.js');
+const post = require('../routes/post.js');
+const reaction = require('../routes/reaction.js');
+const tree = require('../routes/tree.js');
+const treeRequest = require('../routes/treeRequest.js');
+const treeType = require('../routes/treeType.js');
+const appointment = require('../routes/appointment.js');
+const appointmentRequest = require('../routes/appointmentRequest.js');
+const places = require('../routes/place.js');
+const analytics = require('../routes/analytics');
+
+module.exports = app => {
+  app.use('/general', general);
+  app.use('/users', users);
+  app.use('/missions', missions);
+  app.use('/missions_answers', missionsAnswer);
+  app.use('/quizzes', quizzes);
+  app.use('/quiz_answers', quizzesAnswer);
+  app.use('/groups', groups);
+  app.use('/group_members', groupMember);
+  app.use('/posts', post);
+  app.use('/reactions', reaction);
+  app.use('/trees', tree);
+  app.use('/tree_requests', treeRequest);
+  app.use('/tree_types', treeType);
+  app.use('/appointment', appointment);
+  app.use('/appointment_requests', appointmentRequest);
+  app.use('/places', places);
+  app.use('/analytics', analytics);
+};
