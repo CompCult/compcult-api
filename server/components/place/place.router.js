4b34331c5eb9464ce49b6f69fa4e45bbf103d67c
danielrcoura <daniel.coura@ccc.ufcg.edu.br>
2019-02-21 23:28:14 -0300

    Separa rotas dos controllers em place

--- server/components/place/place.router.js
+++ server/components/place/place.router.js
@@ -1,85 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-var Place = require('./place.model');
+const placeCtrl = require('./place.controller');
 
-// Index
-router.get('/', function (req, res) {
-  Place.find({}).catch((err) => {
-    res.status(400).send(err);
-  })
-    .then((places) => {
-      res.status(200).json(places);
-    });
-});
+router.get('/', placeCtrl.findPlaces);
 
-// Find by params
-router.get('/query/fields', function (req, res) {
-  Place.find(req.query, function (err, places) {
-    if (err) {
-      res.status(400).send(err);
-    } else if (!places) {
-      res.status(404).send('Lugar não encontrado');
-    } else {
-      res.status(200).json(places);
-    }
-  });
-});
+router.get('/query/fields', placeCtrl.findPlaceByParams);
 
-// Create
-router.post('/', function (req, res) {
-  var place = new Place();
-  place.name = req.body.name;
-  place.updated_at = new Date();
+router.post('/', placeCtrl.createPlace);
 
-  place.save(function (err) {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(200).send(place);
-    }
-  });
-});
+router.post('/update/:place_id', placeCtrl.updatePlace);
 
-// Update
-router.post('/update/:place_id', function (req, res) {
-  Place.findById(req.params.place_id, function (err, place) {
-    if (!place || err) {
-      res.status(400).send('Lugar não encontrado!');
-    } else {
-      if (req.body.name) place.name = req.body.name;
-      place.updated_at = new Date();
+router.post('/remove/:place_id', placeCtrl.deletePlace);
 
-      place.save(function (err) {
-        if (err) {
-          res.status(400).send(err);
-        } else {
-          res.status(200).send(place);
-        }
-      });
-    }
-  });
-});
-
-// Delete
-router.post('/remove/:place_id', function (req, res) {
-  Place.remove({ _id: req.params.place_id }, function (err) {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(200).send('Lugar removido.');
-    }
-  });
-});
-
-router.delete('/:place_id', function (req, res) {
-  Place.remove({ _id: req.params.place_id }, function (err) {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(200).send('Lugar removido.');
-    }
-  });
-});
+router.delete('/:place_id', placeCtrl.deletePlace);
 
 module.exports = router;
