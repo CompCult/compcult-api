9bda4ec34fa7217a288524155f6ba78973f01dd6
danielrcoura <daniel.coura@ccc.ufcg.edu.br>
2019-02-22 10:09:12 -0300

    Separa rotas dos controllers em reaction

--- server/components/reaction/reaction.router.js
+++ server/components/reaction/reaction.router.js
@@ -1,75 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-var Reaction = require('./reaction.model');
+const reactionController = require('./reaction.controller');
 
-// Index
-router.get('/', function (req, res) {
-  Reaction.find({}, function (err, reactions) {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(200).json(reactions);
-    }
-  });
-});
+router.get('/', reactionController.listReactions);
 
-// Find by params
-router.get('/query/fields', function (req, res) {
-  Reaction.find(req.query, function (err, reaction) {
-    if (err) {
-      res.status(400).send(err);
-    } else if (!reaction) {
-      res.status(404).send('Reação não encontrada');
-    } else {
-      res.status(200).json(reaction);
-    }
-  });
-});
+router.get('/query/fields', reactionController.findReactionByParams);
 
-// Like
-router.post('/', function (req, res) {
-  Reaction.findOne({ _user: req.body._user, _post_reacted: req.body._post_reacted }, function (err, reaction) {
-    if (err) throw err;
+router.post('/', reactionController.addLike);
 
-    if (reaction) {
-      console.log('update');
-      reaction.points += req.body.points;
-
-      reaction.save(function (err) {
-        if (err) {
-          res.status(400).send(err);
-        } else {
-          res.status(200).send('Você reagiu!');
-        }
-      });
-    } else {
-      console.log('create');
-      var react = new Reaction();
-      react._user = req.body._user;
-      react._post_reacted = req.body._post_reacted;
-      react.points = req.body.points;
-
-      react.save(function (err) {
-        if (err) {
-          res.status(400).send(err);
-        } else {
-          res.status(200).send(react);
-        }
-      });
-    }
-  });
-});
-
-// Delete
-router.delete('/:reaction_id', function (req, res) {
-  Reaction.remove({ _id: req.params.reaction_id }, function (err) {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(200).send('Reação removida.');
-    }
-  });
-});
+router.delete('/:reaction_id', reactionController.deleteReaction);
 
 module.exports = router;
