e0e139c8cd6c2fc6f37793ee26e5f4d7c97ca8c0
italomiguelpinheiro <italo.pinheiro@ccc.ufcg.edu.br>
2019-11-22 12:56:25 -0300

    retorna quiz que o usario não respondeu

--- src/components/quiz/quiz.model.js
+++ src/components/quiz/quiz.model.js
@@ -2,11 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.Types.ObjectId;
 
+
 const Quiz = new Schema({
+
+ 
   _user: {
     type: ObjectId,
     ref: 'User'
   },
+   
+  users: {
+  type:[{type: ObjectId, ref: 'User'}]
+  },
+
   title: String,
   description: String,
   points: Number,
