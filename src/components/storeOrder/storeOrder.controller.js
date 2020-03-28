a038fcbb8ff596f3e5cd4717ce5329c213bc5284
izaquielnn <izaquiel.nascimento@ccc.ufcg.edu.br>
2020-03-28 13:58:43 -0300

    query orders by status

--- src/components/storeOrder/storeOrder.controller.js
+++ src/components/storeOrder/storeOrder.controller.js
@@ -1,10 +1,14 @@
-const {StoreOrder, orderStatus} = require('./storeOrder.model');
-const {User, userTypes} = require('../user/user.model');
+const { StoreOrder, orderStatus } = require('./storeOrder.model');
+const { User, userTypes } = require('../user/user.model');
 const StoreItem = require('../storeItem/storeItem.model');
 const _ = require('lodash');
 
 exports.listOrders = async (req, res) => {
-  let query = {_item: req.item.id};
+  let query = { _item: req.item.id };
+
+  if (Object.keys(req.query).includes('status')) {
+    query.status = req.query['status'];
+  }
 
   const orders = await StoreOrder.find(query)
     .populate({ path: '_user', select: 'name' });
@@ -12,29 +16,29 @@ exports.listOrders = async (req, res) => {
   return res.send(orders);
 };
 
-exports.createOrder= async (req, res) => {
+exports.createOrder = async (req, res) => {
   const order = new StoreOrder(req.body);
   order._user = req.user.id;
   order._item = req.item.id;
 
-  if(req.item.users.includes(req.user.id))
-    return res.status(409).send('User has already purchased this item!'); 
-  
-  if(req.item.quantity * order.quantity <= 0) 
-    return res.status(409).send('Insufficient item quantity!'); 
+  if (req.item.users.includes(req.user.id))
+    return res.status(409).send('User has already purchased this item!');
+
+  if (req.item.quantity * order.quantity <= 0)
+    return res.status(409).send('Insufficient item quantity!');
 
   const user = await User.findById(req.user.id);
 
   console.log(user.resources);
 
-  if(user.resources < req.item.value * order.quantity)
+  if (user.resources < req.item.value * order.quantity)
     return res.status(409).send('Insufficient user resources');
 
   user.resources -= req.item.value * order.quantity;
   req.item.quantity -= order.quantity;
   req.item.users.push(user.id);
 
-  
+
 
   await order.save();
   await user.save();
@@ -45,13 +49,13 @@ exports.createOrder= async (req, res) => {
 exports.updateOrder = async (req, res) => {
 
 
-  if(req.body.status && req.order.status == orderStatus.REJECTED)
-      return res.status(409).send('Cannot change a rejected order');
+  if (req.body.status && req.order.status == orderStatus.REJECTED)
+    return res.status(409).send('Cannot change a rejected order');
 
-  if(req.body.status ==  orderStatus.REJECTED){
+  if (req.body.status == orderStatus.REJECTED) {
 
     const item = await StoreItem.findById(req.order._item);
-    item.set({'quantity': item.quantity + req.order.quantity});
+    item.set({ 'quantity': item.quantity + req.order.quantity });
     item.users.pull(req.order._user);
     await item.save();
 
